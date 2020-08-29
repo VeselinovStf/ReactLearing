bd08a33b11df28b118e686fda41553050c34be32
Stv <veselinoStf@gmail.com>
2020-08-29 08:52:45 +0300

    initial projects from local env

---
+++ author-quiz-app/src/AuthorQuiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function AuthorQuiz() {
+  return (
+    <div className="App">
+     Wellcome to Author Quiz
+    </div>
+  );
+}
+
+export default AuthorQuiz;
