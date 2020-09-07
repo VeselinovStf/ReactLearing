45a8cf9a487d955988de677bae6d231c3c9d54f8
veselinovStf <veselinovStf@gmail.com>
2020-09-07 07:47:31 +0300

    Final Environment Project

--- build-apps-with-react-redux/src/index.js
+++ build-apps-with-react-redux/src/index.js
@@ -1,2 +1,8 @@
 import React from "react";
-import { render } from "enzyme";
+import { render } from "react-dom";
+
+function Work() {
+  return <h1>Work</h1>;
+}
+
+render(<Work />, document.getElementById("app"));
